0d1f1e0e0888027d748589932bb47eef115957f5
[Flutter_SakuSaku13:44] <[gitonly543@gmail.com]>
2023-04-15 21:18:05 +0900

    add 例外処理を追加

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:sub_scription_app/main.dart';
+import 'package:sub_scription_app/service/sign_in_service.dart';
+import 'package:sub_scription_app/ui/auth/button_freame.dart';
+import 'package:sub_scription_app/ui/auth/sign_up_page.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    TextEditingController email = TextEditingController();
+    TextEditingController password = TextEditingController();
+    // 認証機能を使用するクラス
+    final signInService = SignInService();
+
     return SafeArea(
       child: Scaffold(
         body: Column(
@@ -16,6 +25,7 @@ class LoginPage extends StatelessWidget {
                 "https://www.chargebee.com/blog/wp-content/uploads/2022/07/Chargebee-Subscription-Box-Industry-trends-opportunities-and-Market-Size.png"),
             Text("LOGIN"),
             TextFormField(
+              controller: email,
               decoration: InputDecoration(
                 hintText: 'メールアドレスを入力してください',
                 fillColor: Colors.orange[100],
@@ -29,6 +39,7 @@ class LoginPage extends StatelessWidget {
               ),
             ),
             TextFormField(
+              controller: password,
               obscureText: true,
               decoration: InputDecoration(
                 hintText: 'パスワードを入力してください',
@@ -45,36 +56,40 @@ class LoginPage extends StatelessWidget {
             ButtonFlame(
               label: "LOGIN",
               colors: Colors.blue,
-              onPressed: () {},
+              onPressed: () async {
+                try {
+                  await signInService.signIn(email.text, password.text);
+                  // 認証に成功した場合の処理
+                  if (context.mounted) {
+                    Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const FirstPage()),
+                        (route) => false);
+                  }
+                } catch (e) {
+                  _showErrorSnackbar(context, e.toString());
+                }
+              },
             ),
             ButtonFlame(
               label: "REGISTER",
               colors: Colors.red,
-              onPressed: () {},
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const SignUpPage()));
+              },
             ),
           ],
         ),
       ),
     );
   }
-}
 
-class ButtonFlame extends StatelessWidget {
-  const ButtonFlame(
-      {required this.label, required this.onPressed, this.colors, super.key});
-
-  final String label;
-  final VoidCallback onPressed;
-  final Color? colors;
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      style: ButtonStyle(
-        backgroundColor: MaterialStateProperty.all(colors),
-      ),
-      onPressed: () {},
-      child: Text(label),
+  void _showErrorSnackbar(BuildContext context, String message) {
+    final snackBar = SnackBar(
+      content: Text(message),
+      duration: const Duration(seconds: 3),
     );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 }
--- lib/service/platform_service.dart
+++ lib/service/platform_service.dart
@@ -7,10 +7,17 @@ class PlatformService {
   final createdAt = Timestamp.fromDate(DateTime.now());
   // 新規登録でプラットフォーム名を登録するメソッド
   Future<void> addPlatFormName(String pf_name) async {
-    // uidを使用する
-    final uid = auth.currentUser?.uid;
-    await db.collection('platform').doc(uid).set({
-      'pf_name': pf_name,
-    });
+    try {
+      if(pf_name.isEmpty) {
+        throw('名前が入力されていません!');
+      }
+      // uidを使用する
+      final uid = auth.currentUser?.uid;
+      await db.collection('platform').doc(uid).set({
+        'pf_name': pf_name,
+      });
+    } catch (e) {
+      throw(e.toString());
+    }
   }
 }
