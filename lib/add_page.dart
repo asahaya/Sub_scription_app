36f74863d864250dd1cb93b8e984d7d57a7e99f6
[Flutter_SakuSaku13:44] <[gitonly543@gmail.com]>
2023-04-15 22:04:12 +0900

    add テスト用にログアウト機能を実装

--- lib/add_page.dart
+++ lib/add_page.dart
@@ -1,12 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:sub_scription_app/login_page.dart';
 import 'package:sub_scription_app/main.dart';
 
 import 'common.dart';
 
-
- 
 class AddPage extends StatefulWidget {
   const AddPage({super.key});
 
@@ -15,55 +15,67 @@ class AddPage extends StatefulWidget {
 }
 
 class _AddPageState extends State<AddPage> {
-
-String? isSelection='Movie';
+  String? isSelection = 'Movie';
 
   @override
   Widget build(BuildContext context) {
+    final auth = FirebaseAuth.instance;
+
     return Scaffold(
       appBar: AppBar(
-        title:const Text('プランを追加する'),
+        title: const Text('プランを追加する'),
         actions: [
-          IconButton(onPressed: (){
-
-          }, icon: Icon(Icons.check))
+          Row(
+            children: [
+              // 一旦ログアウトボタンを配置
+              IconButton(
+                  onPressed: () async {
+                    await auth.signOut();
+                    if(context.mounted) {
+                    Navigator.pushAndRemoveUntil(
+                        context, MaterialPageRoute(builder: (context) => LoginPage()), (route) => false);
+                    }
+                  },
+                  icon: Icon(Icons.logout)),
+              IconButton(onPressed: () {}, icon: Icon(Icons.check)),
+            ],
+          )
         ],
       ),
       body: Column(
         children: [
           Row(
             children: [
-            Expanded(child: Image.network(img)),
+              Expanded(child: Image.network(img)),
               Expanded(
                 child: Column(
                   children: [
                     DropdownButton(
-                      items: const[
-                      DropdownMenuItem(
-                        child: Text('Movie'),
-                        value: 'Movie',
-                      ),
-                      DropdownMenuItem(
-                        child: Text('Audio'),
-                        value: 'Audio',
-                      ),
-                      DropdownMenuItem(
-                        child: Text('Skill'),
-                        value: 'Skill',
-                      ),
-                      DropdownMenuItem(
-                        child: Text('other'),
-                        value: 'other',
-                      ),
-                    ],
-                     onChanged: (String? value){
-                      setState(() {
-                        isSelection=value;
-                      });
-                    },
-                    value: isSelection,
+                      items: const [
+                        DropdownMenuItem(
+                          child: Text('Movie'),
+                          value: 'Movie',
+                        ),
+                        DropdownMenuItem(
+                          child: Text('Audio'),
+                          value: 'Audio',
+                        ),
+                        DropdownMenuItem(
+                          child: Text('Skill'),
+                          value: 'Skill',
+                        ),
+                        DropdownMenuItem(
+                          child: Text('other'),
+                          value: 'other',
+                        ),
+                      ],
+                      onChanged: (String? value) {
+                        setState(() {
+                          isSelection = value;
+                        });
+                      },
+                      value: isSelection,
                     ),
-                    
                     TextFormField(),
                     TextFormField(),
                   ],
@@ -71,7 +83,6 @@ String? isSelection='Movie';
               ),
             ],
           ),
-
           TitleFlame(title: '開始日'),
         ],
       ),
@@ -80,17 +91,19 @@ String? isSelection='Movie';
 }
 
 class TitleFlame extends StatelessWidget {
-  const TitleFlame({required this.title,super.key});
- final String title;
+  const TitleFlame({required this.title, super.key});
+  final String title;
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
-        height: 50,
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.grey,),
+    return Container(
+      height: 50,
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.grey,
         ),
-        child: Center(child: Text(title)),
+      ),
+      child: Center(child: Text(title)),
     );
   }
 }
