8e8fee708f63b86de3c1260e3469bb6ac6954544
[Flutter_SakuSaku13:44] <[gitonly543@gmail.com]>
2023-04-15 16:43:42 +0900

    add CLIをインポート

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:sub_scription_app/add_page.dart';
+import 'package:sub_scription_app/firebase_options.dart';
 
 import 'common.dart';
-
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
