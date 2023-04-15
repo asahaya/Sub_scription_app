e6430a3f08679941c651c5b6326331d1d8dfa096
asahaya <asa7kadensony@gmail.com>
2023-04-15 17:07:00 +0900

    loginPage

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -7,19 +7,74 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          ElevatedButton(
-            onPressed: (){},
-           child: Text('LOGIN'),
-           ),
-           ElevatedButton(
-            onPressed: (){},
-           child: Text('REGISTER'),
-           ),
-        ],
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Image.network(
+                "https://www.chargebee.com/blog/wp-content/uploads/2022/07/Chargebee-Subscription-Box-Industry-trends-opportunities-and-Market-Size.png"),
+            Text("LOGIN"),
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: 'メールアドレスを入力してください',
+                fillColor: Colors.orange[100],
+                filled: true,
+                isDense: true,
+                prefixIcon: const Icon(Icons.man),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(32),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+            TextFormField(
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: 'パスワードを入力してください',
+                fillColor: Colors.orange[100],
+                filled: true,
+                isDense: true,
+                prefixIcon: const Icon(Icons.password),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(32),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+            ButtonFlame(
+              label: "LOGIN",
+              colors: Colors.blue,
+              onPressed: () {},
+            ),
+            ButtonFlame(
+              label: "REGISTER",
+              colors: Colors.red,
+              onPressed: () {},
+            ),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class ButtonFlame extends StatelessWidget {
+  const ButtonFlame(
+      {required this.label, required this.onPressed, this.colors, super.key});
+
+  final String label;
+  final VoidCallback onPressed;
+  final Color? colors;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(colors),
+      ),
+      onPressed: () {},
+      child: Text(label),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sub_scription_app/add_page.dart';
+import 'package:sub_scription_app/login_page.dart';
 
 import 'common.dart';
 
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const FirstPage(),
+      home: const LoginPage(),
     );
   }
 }
