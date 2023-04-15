6468a897bf4aa1ba822f8e1e63c04976955129ae
[Flutter_SakuSaku13:44] <[gitonly543@gmail.com]>
2023-04-15 21:17:00 +0900

    change VoidCallBackを引数に追加

---
+++ lib/ui/auth/button_freame.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ButtonFlame extends StatelessWidget {
+  const ButtonFlame(
+      {required this.label, required this.onPressed, this.colors, super.key});
+
+  final String label;
+  final VoidCallback onPressed;
+
+  final Color? colors;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(colors),
+      ),
+      onPressed: onPressed,
+      child: Text(label),
+    );
+  }
+}
